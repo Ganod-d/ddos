652403fcf8119eff70addeed53405f2d70a123c3
Ganod-d <20010946@st.phenikaa-uni.edu.vn>
2024-07-16 11:38:54 +0700

    first commit 10

--- pyddos.py
+++ pyddos.py
@@ -1,66 +1,28 @@
-import random
 import requests
-
-# Đọc dữ liệu từ proxy.txt
-def read_proxy_file(filename):
-    proxies = []
-    try:
-        with open(filename, 'r') as file:
-            proxies = file.read().splitlines()
-    except FileNotFoundError:
-        print(f"File {filename} not found.")
-    return proxies
-
-# Đọc dữ liệu từ ua.txt
-def read_user_agents_file(filename):
-    user_agents = []
-    try:
-        with open(filename, 'r') as file:
-            user_agents = file.read().splitlines()
-    except FileNotFoundError:
-        print(f"File {filename} not found.")
-    return user_agents
-
-# Chọn ngẫu nhiên một proxy và một user agent
-def get_random_proxy(proxies):
-    return random.choice(proxies)
-
-def get_random_user_agent(user_agents):
-    return random.choice(user_agents)
 
 # URL mẫu để thử nghiệm
 url = 'https://httpbin.org/user-agent'
 
-# Đọc dữ liệu từ các file
-proxy_file = 'proxy.txt'
-ua_file = 'ua.txt'
+# Proxy và User Agent cụ thể
+proxy = '103.41.32.182:58080'
+user_agent = 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0'
 
-proxies = read_proxy_file(proxy_file)
-user_agents = read_user_agents_file(ua_file)
+# Tạo session requests với proxy và user agent cụ thể
+session = requests.Session()
+session.proxies = {'http': f'http://{proxy}', 'https': f'http://{proxy}'}
+session.headers = {'User-Agent': user_agent}
 
-if proxies and user_agents:
-    # Chọn ngẫu nhiên một proxy và một user agent
-    proxy = get_random_proxy(proxies)
-    user_agent = get_random_user_agent(user_agents)
+try:
+    # Gửi yêu cầu HTTP GET với proxy và user agent đã chọn
+    response = session.get(url)
     
-    # Tạo session requests với proxy và user agent
-    session = requests.Session()
-    session.proxies = {'http': proxy, 'https': proxy}
-    session.headers = {'User-Agent': user_agent}
-    
-    try:
-        # Gửi yêu cầu HTTP GET với proxy và user agent đã chọn
-        response = session.get(url)
-        
-        # Kiểm tra và in kết quả
-        if response.status_code == 200:
-            print(f"Proxy: {proxy}")
-            print(f"User-Agent: {user_agent}")
-            print("Response:")
-            print(response.text)
-        else:
-            print(f"Failed to retrieve data. Status code: {response.status_code}")
-    except requests.RequestException as e:
-        print(f"Request error: {e}")
-else:
-    print("Không có dữ liệu proxy hoặc user agent để thử nghiệm.")
+    # Kiểm tra và in kết quả
+    if response.status_code == 200:
+        print(f"Proxy: {proxy}")
+        print(f"User-Agent: {user_agent}")
+        print("Response:")
+        print(response.text)
+    else:
+        print(f"Failed to retrieve data. Status code: {response.status_code}")
+except requests.RequestException as e:
+    print(f"Request error: {e}")
