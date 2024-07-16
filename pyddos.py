ef14d2bbe6088e2a279fa33962f6a972da26bc11
Ganod-d <20010946@st.phenikaa-uni.edu.vn>
2024-07-16 11:41:27 +0700

    first commit 12

--- pyddos.py
+++ pyddos.py
@@ -1,11 +1,15 @@
 import requests
 
-# URL mẫu để thử nghiệm
+# URL mẫu để thử nghiệm (ví dụ là một URL có thể bị từ chối truy cập)
 url = 'https://4gchill.com'
 
 # Proxy và User Agent cụ thể
 proxy = '103.41.32.182:58080'
 user_agent = 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0'
+
+# Hiển thị thông báo sử dụng proxy và user agent
+print(f"Đang sử dụng Proxy: {proxy}")
+print(f"Đang sử dụng User-Agent: {user_agent}")
 
 # Tạo session requests với proxy và user agent cụ thể
 session = requests.Session()
@@ -16,10 +20,8 @@ try:
     # Gửi yêu cầu HTTP GET với proxy và user agent đã chọn
     response = session.get(url)
     
-    # Kiểm tra và in kết quả
+    # Kiểm tra mã trạng thái của response
     if response.status_code == 200:
-        print(f"Proxy: {proxy}")
-        print(f"User-Agent: {user_agent}")
         print("Response:")
         print(response.text)
     else:
